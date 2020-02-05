193d5a6fbcf9ee350794345ff79c6baf75336b87
serallab <sesikanth26@gmail.com>
2020-02-05 15:34:59 +0530

    message

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import otherpage from './otherpage';
+import Fib from './fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other</Link>
+        </header>
+        <div>
+              <Route exact path='/' component={Fib}/>
+              <Route exact path='/otherpage' component={otherpage}/>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
