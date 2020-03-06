8a142b186abfa847003d0b5a904f2bb4a0fca2fd
serallab <serallab@cisco.com>
2020-03-06 14:59:08 +0530

    corrected postgres deploy

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,6 @@ docker push sesikanth/multi-worker:${SHA}
 docker push sesikanth/multi-server:latest
 docker push sesikanth/multi-server:${SHA}
 kubectl apply -f k8s
-kubectl apply -f k8s/postgres-deployment
 kubectl set image deployments/server-deployment server=sesikanth/multi-server:${SHA}
 kubectl set image deployments/client-deployment client=sesikanth/multi-client:${SHA}
 kubectl set image deployments/worker-deployment worker=sesikanth/multi-worker:${SHA}
